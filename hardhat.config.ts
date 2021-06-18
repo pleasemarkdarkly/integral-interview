cc6232a4090e463f93b62d7ec6981dcc1839f272
Mark Phillips <mark.phillips@protonmail.com>
2021-06-18 14:56:38 -0700

    fix: 🐛 updated bogus private key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -36,7 +36,7 @@ if (!mnemonic) {
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
 const INFURA_API_KEY = process.env.INFURA_API_KEY || '';
 const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL || '';
-const ETH_PRIVATE_KEY = process.env.ETH_PRIVATE_KEY || '5450ef8cc4b91ed61a8c393d46e32f9928becda6a2abc8a354b0d60a1abda734';
+const ETH_PRIVATE_KEY = process.env.ETH_PRIVATE_KEY || '5450ef8cc4b91ed61a8c393d46e55f9928becda6a2abc8a354b0d60a1abda436';
 
 export class HardhatRuntimeEnvironmentField {
   constructor() { null }
